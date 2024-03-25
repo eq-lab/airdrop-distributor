f5858fe01900acb52ae8cd0bd9666905982e21d6
Pavel <pavel.smelov@equilibrium.io>
2024-03-25 19:07:38 +0300

    add optional test token contract deployment

--- contracts/test/MintableERC20.sol
+++ contracts/test/MintableERC20.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.19;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/access/Ownable2Step.sol';
 
 /// @dev Mintable ERC20 token.
-contract MintableERC20 is ERC20 {
+contract MintableERC20 is ERC20, Ownable2Step {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
   function mint(address account, uint256 amount) external {
--- deploy/src.ts
+++ deploy/src.ts
@@ -1,14 +1,25 @@
 import { ethers } from 'ethers';
 import { handle } from '@oclif/errors';
 import { Command, flags } from '@oclif/command';
-import { AirdropDistributor__factory } from '../typechain-types';
+import { AirdropDistributor__factory, MintableERC20__factory } from '../typechain-types';
 import { formatUnits } from 'ethers/lib/utils';
 import ganache from 'ganache';
 import fs from 'fs';
 import path from 'path';
 
-type AirdropConfig = { nodeUri: string; airdropTokenAddress: string; airdropTokenStorageAddress: string };
-type DeploymentData = { address: string; txHash: string };
+type AirdropConfig = {
+  nodeUri: string;
+  airdropTokenAddress?: string;
+  testToken?: TestTokenConfig;
+  airdropTokenStorageAddress: string;
+};
+type TestTokenConfig = { name: string; symbol: string };
+
+type ContractDeploymentData = { address: string; txHash: string };
+type DeploymentData = {
+  airdropContract: ContractDeploymentData;
+  token?: ContractDeploymentData;
+};
 
 class AirdropDeployment extends Command {
   static description = 'Airdrop contract deployment';
@@ -48,27 +59,64 @@ class AirdropDeployment extends Command {
     const initialBalance = await deployer.getBalance();
     console.log(`Deployer initial balance: ${formatUnits(initialBalance, 18)} ETH`);
 
-    const airdropContractFactory = new AirdropDistributor__factory();
-    const airdropContract = await airdropContractFactory
-      .connect(deployer)
-      .deploy(configData.airdropTokenAddress, configData.airdropTokenStorageAddress);
-    await airdropContract.deployed();
+    const testTokenDataPresent = configData.testToken !== undefined;
+    const tokenAddressPresent = configData.airdropTokenAddress !== undefined;
 
-    const deploymentData: DeploymentData = {
-      address: airdropContract.address,
-      txHash: airdropContract.deployTransaction.hash,
-    };
-    console.log(`\nContract deployed:`);
-    console.log(`  address: ${deploymentData.address}`);
-    console.log(`  tx hash: ${deploymentData.txHash}`);
+    if (testTokenDataPresent && tokenAddressPresent) {
+      throw new Error(`Both airdropTokenAddress and testToken are provided in config. Pls remove one of them`);
+    }
+
+    if (!testTokenDataPresent && !tokenAddressPresent) {
+      throw new Error(`Both airdropTokenAddress and testToken are absent in config. Pls add one of them`);
+    }
+
+    const tokenDeploymentData = testTokenDataPresent
+      ? await this.deployToken(deployer, configData.testToken!)
+      : undefined;
+    const tokenAddress = tokenAddressPresent ? configData.airdropTokenAddress! : tokenDeploymentData!.address;
+
+    const airdropDeploymentData = await this.deployAirdropContract(
+      deployer,
+      tokenAddress,
+      configData.airdropTokenStorageAddress
+    );
 
     const resultingBalance = await deployer.getBalance();
     console.log(`\nDeployer resulting balance: ${formatUnits(resultingBalance, 18)} ETH`);
     console.log(`Spent: ${formatUnits(initialBalance.sub(resultingBalance), 18)} ETH`);
 
+    const deploymentData: DeploymentData = { airdropContract: airdropDeploymentData, token: tokenDeploymentData };
     if (!dryRun) {
       this.saveDeploymentData(deploymentData, this.getDir(config));
     }
+  }
+
+  async deployAirdropContract(
+    deployer: ethers.Signer,
+    tokenAddress: string,
+    tokenStorage: string
+  ): Promise<ContractDeploymentData> {
+    console.log(`\nDeploying airdrop contract`);
+    const airdropContractFactory = new AirdropDistributor__factory();
+    const airdropContract = await airdropContractFactory.connect(deployer).deploy(tokenAddress, tokenStorage);
+    await airdropContract.deployed();
+
+    console.log(`\nAirdrop contract deployed:`);
+    console.log(`  address: ${airdropContract.address}`);
+    console.log(`  tx hash: ${airdropContract.deployTransaction.hash}`);
+    return { address: airdropContract.address, txHash: airdropContract.deployTransaction.hash };
+  }
+
+  async deployToken(deployer: ethers.Signer, config: TestTokenConfig): Promise<ContractDeploymentData> {
+    console.log(`\nDeploying token contract`);
+    const tokenFactory = new MintableERC20__factory();
+    const token = await tokenFactory.connect(deployer).deploy(config.name, config.symbol);
+    await token.deployed();
+
+    console.log(`\nToken contract deployed:`);
+    console.log(`  address: ${token.address}`);
+    console.log(`  tx hash: ${token.deployTransaction.hash}`);
+    return { address: token.address, txHash: token.deployTransaction.hash };
   }
 
   async getProvider(nodeUri: string, dryRun: boolean): Promise<ethers.providers.JsonRpcProvider> {
@@ -95,7 +143,7 @@ class AirdropDeployment extends Command {
   readConfig(configPath: string): AirdropConfig {
     const config: AirdropConfig = require(configPath);
 
-    if (!ethers.utils.isAddress(config.airdropTokenAddress)) {
+    if (config.airdropTokenAddress !== undefined && !ethers.utils.isAddress(config.airdropTokenAddress)) {
       throw new Error(`airdropTokenAddress has invalid value: ${config.airdropTokenAddress}`);
     }
 
